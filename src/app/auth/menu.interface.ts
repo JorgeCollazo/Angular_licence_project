2af11ff69099788bd5069308a3bee43ba96a11b4
JorgeCC <jorgecclucy@gmail.com>
2023-10-26 15:46:05 -0500

    added menu.interface

---
+++ src/app/auth/menu.interface.ts
@@ -0,0 +1,13 @@
+export interface IMenu {
+    idmenu: number,
+    estado: number,
+    crear: number,
+    editar: number,
+    eliminar: number,
+    ver: number,
+    principal: number,
+    menu: string,
+    link: string,
+    orden: number,
+    muestra: number,
+  }
